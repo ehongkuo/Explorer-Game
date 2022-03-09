4060b74ce07d60a6a31848722f8cc587a1a32fff
Ehong Kuo <ehongkuo@gmail.com>
2022-03-09 15:12:25 -0800

    First commit

---
+++ zip_submission.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+zip submission.zip \
+    building.cpp \
+    building.hpp \
+    controller.hpp \
+    curses_controller.cpp \
+    curses_controller.hpp \
+    curses_view.cpp \
+    curses_view.hpp \
+    game.cpp \
+    game.hpp \
+    interface_type.hpp \
+    item.cpp \
+    item.hpp \
+    map_segment.cpp \
+    map_segment.hpp \
+    print_controller.cpp \
+    print_controller.hpp \
+    print_view.cpp \
+    print_view.hpp \
+    view.cpp \
+    view.hpp
